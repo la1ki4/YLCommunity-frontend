d36c7d7461f513254ed8ea7153cdffb3aaeb4feb
Alexandros Papas <alexandros.papas@mail.ru>
2025-11-11 17:58:37 +0200

    add new post changes

--- src/widgets/CreationPostBlock/CreationPostBlock.jsx
+++ src/widgets/CreationPostBlock/CreationPostBlock.jsx
@@ -6,21 +6,25 @@ import textStyles from "@app/styles/text.module.css"
 import blockFormStyle from "@app/styles/block.module.css"
 import inputStyle from "@app/styles/input.module.css"
 import {Image} from "@shared/Image/Image.jsx";
+import pictureStyle from "@app/styles/picture.module.css"
 import NewPostIcon from "@app/assets/New_post.svg"
+import postStyles from "@app/styles/post.module.css";
 
 export function CreationPostBlock() {
     return (
         <Block className={`${blockFormStyle.blockContainer} ${blockFormStyle.blockPadding}`}>
-            <Text text={"What's new?"} className={textStyles.bigTitle}></Text>
-            <Block className={blockFormStyle.blockFile}>
+            <Block className={blockFormStyle.addNewPostBlockDistance}>
+                <Text text={"What's new?"} className={textStyles.bigTitle}></Text>
+            </Block>
+            <Block className={`${blockFormStyle.blockFile}`}>
                 <InputField type={"file"} className={
                     `${inputStyle.inputFileButton}`}>
                 </InputField>
-                <Image image={NewPostIcon}></Image>
-                <Text text={"Add new post"}></Text>
+                <Image image={NewPostIcon} className={pictureStyle.addNewPostIcon}></Image>
+                <Text text={"Add new post"} className={textStyles.addNewPostText}></Text>
             </Block>
-            <Block>
-                <InputField></InputField>
+            <Block className={`${postStyles.postAddCommentContainer} ${blockFormStyle.addNewPostBlockDistance}`}>
+                <InputField type="text" placeholder='Say something new ...' className={postStyles.postAddComment}/>
             </Block>
             <Button onClick={Button} type={"submit"} label={"Add post"}></Button>
         </Block>
