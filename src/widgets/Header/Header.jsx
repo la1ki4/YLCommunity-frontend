cc6dfcf71665973413cb4e0e4eeeda30ec90cb71
Alexandros <palexandros2003@yahoo.com>
2026-05-04 14:26:19 +0300

    logout

--- src/widgets/Header/Header.jsx
+++ src/widgets/Header/Header.jsx
@@ -31,7 +31,6 @@ export function Header(){
                         className={`${textStyle.popupTextBlock} ${textStyle.defaultWhiteText}`}
                         as="a"
                         text="Log out"
-                        href="#"
                         onClick={handleLogoutClick}
                     />
                 </ProfilePopup>
