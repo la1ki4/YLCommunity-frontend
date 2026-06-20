c9bd53e972555eee70ea999dc3b9811a91f1c8cb
Alexandros <palexandros2003@yahoo.com>
2026-06-20 13:38:50 +0300

    upd

---
+++ src/features/calendar/hooks/year/useMoreEventsPopupYearCalendarPosition.js
@@ -0,0 +1,127 @@
+import {useCallback, useEffect, useLayoutEffect} from "react";
+
+import {getDayOfWeekShort} from "@features/calendar/utils/calendarDate.utils.js";
+import {DOW} from "@features/calendar/constants/calendar.constants";
+
+export function useMoreEventsPopupPosition({
+                                               view,
+                                               scrollRef,
+                                               dayButtonRefs,
+                                               yearMiniCalendarRef,
+                                               monthCalendarPositionIndex,
+                                               setMonthCalendarPositionIndex,
+                                               setMoreInfoPopupTop,
+                                               setMoreInfoPopupLeft,
+                                           }) {
+
+    const updatePosition = useCallback(() => {
+        if (!view) {
+            return;
+        }
+
+        const key =
+            `${view.year}-${view.monthIndex}-${view.day}`;
+
+        const button =
+            dayButtonRefs.current[key];
+
+        const yearMiniCalendarNode =
+            yearMiniCalendarRef.current;
+
+        const dow = getDayOfWeekShort(view);
+        const dowIndex = DOW.indexOf(dow);
+
+        if (
+            !button ||
+            !yearMiniCalendarNode
+        ) {
+            return;
+        }
+
+        let positionIndex;
+
+        switch (true) {
+            case window.innerWidth > 1700:
+                positionIndex = view.monthIndex % 4;
+                break;
+
+            case window.innerWidth > 1500:
+                positionIndex = view.monthIndex % 3;
+                break;
+
+            case window.innerWidth > 900:
+                positionIndex = view.monthIndex % 2;
+                break;
+
+            default:
+                positionIndex = 0;
+        }
+
+        if (
+            positionIndex !==
+            monthCalendarPositionIndex
+        ) {
+            setMonthCalendarPositionIndex(
+                positionIndex
+            );
+        }
+
+        const buttonRect =
+            button.getBoundingClientRect();
+
+        const yearMiniCalendarRect =
+            yearMiniCalendarNode.getBoundingClientRect();
+
+        const yearCalendarPos =
+            (yearMiniCalendarRect.width + 18) *
+            positionIndex;
+
+        const yearWeekCalendarButtonPos =
+            (yearMiniCalendarRect.width / 7) *
+            dowIndex;
+
+        setMoreInfoPopupTop(
+            buttonRect.top - buttonRect.height
+        );
+
+        setMoreInfoPopupLeft(
+            -200 +
+            40 +
+            yearCalendarPos +
+            yearWeekCalendarButtonPos
+        );
+    }, [
+        view,
+        dayButtonRefs,
+        yearMiniCalendarRef,
+        monthCalendarPositionIndex,
+        setMonthCalendarPositionIndex,
+        setMoreInfoPopupTop,
+        setMoreInfoPopupLeft,
+    ]);
+
+    useLayoutEffect(() => {
+        updatePosition();
+    }, [updatePosition]);
+
+    useEffect(() => {
+        const node = scrollRef.current;
+
+        if (!node) {
+            return;
+        }
+
+        node.addEventListener(
+            "scroll",
+            updatePosition,
+            { passive: true }
+        );
+
+        return () => {
+            node.removeEventListener(
+                "scroll",
+                updatePosition
+            );
+        };
+    }, [scrollRef, updatePosition]);
+}
